62344afe696ef70b0cc92e54d29d6612f43d8db2
MgtaDev <passantinodev@gmail.com>
2024-07-31 14:13:49 -0400

    d

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ElBlog from '../components/blog'
+import Sidebar from '../components/sidebar'
 
 const Blog = () => {
   return (
@@ -170,113 +171,7 @@ const Blog = () => {
         </div>
         {/* <!-- End Content --> */}
     
-        {/* <!-- Sidebar --> */}
-        <div class="lg:col-span-2  lg:right-0 lg:top-14 relative ml-6 lg:ml-0 lg:mr-4 lg:h-[550px] mt-0 rounded-lg lg:bg-gradient-to-r lg:from-gray-50 lg:via-transparent lg:to-transparent dark:from-neutral-800 ">
-          <div class="sticky top-0 start-0  py-8 lg:ps-8">
-            {/* <!-- Avatar Media --> */}
-            <div class="group flex items-center gap-x-3 border-b border-gray-200 pb-8 mb-8 dark:border-neutral-700">
-              <a class="block shrink-0 focus:outline-none" href="#">
-                <img class="size-10 rounded-full" src="/photo_2023-12-1817.45.05.jpeg" alt="Avatar"/>
-              </a>
-    
-              <a class="group grow block focus:outline-none" href="">
-                <h5 class="group-hover:text-gray-600 group-focus:text-gray-600 text-sm font-semibold text-gray-800 dark:group-hover:text-neutral-400 dark:group-focus:text-neutral-400 dark:text-neutral-200">
-                  Daniel Passantino
-                </h5>
-                <p class="text-sm text-gray-500 dark:text-neutral-500">
-                  Web Developer & Marketing CEO
-                </p>
-              </a>
-    
-              <div class="grow">
-                <div class="flex">
-                  <img class="size-8 rounded-full" src="/whatsapp.png" alt="Avatar"/>
-                </div>
-              </div>
-            </div>
-    
-            <div class="pr-4 lg:-ml-1">
-                <div className='space-y-6 overflow-y-scroll h-[40vh] min-h-[400px]'>
-                <h4>Lee otros articulos:</h4>
-              <a class="group flex items-center gap-x-6 focus:outline-none" href="/blog/articulo-1">
-                <div class="grow">
-                  <span class="text-sm font-bold text-gray-800 group-hover:text-blue-600 group-focus:text-blue-600 dark:text-neutral-200 dark:group-hover:text-blue-500 dark:group-focus:text-blue-500">
-                    5 Reasons to Not start a UX Designer Career in 2022/2023
-                  </span>
-                </div>
-    
-                <div class="shrink-0 relative mr-4 rounded-lg overflow-hidden size-20">
-                  <img class="size-full absolute top-0 start-0 object-cover rounded-lg" src="https://images.unsplash.com/photo-1567016526105-22da7c13161a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80" alt="Blog Image"/>
-                </div>
-              </a>
-    
-              <a class="group flex items-center gap-x-6 focus:outline-none" href="/blog/articulo-2">
-                <div class="grow">
-                  <span class="text-sm font-bold text-gray-800 group-hover:text-blue-600 group-focus:text-blue-600 dark:text-neutral-200 dark:group-hover:text-blue-500 dark:group-focus:text-blue-500">
-                    If your UX Portfolio has this 20% Well Done, it Will Give You an 80% Result
-                  </span>
-                </div>
-    
-                <div class="shrink-0 relative  mr-4  rounded-lg overflow-hidden size-20">
-                  <img class="size-full absolute top-0 start-0 object-cover rounded-lg" src="https://images.unsplash.com/photo-1542125387-c71274d94f0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80" alt="Blog Image"/>
-                </div>
-              </a>
-    
-              <a class="group flex items-center gap-x-6 focus:outline-none" href="/blog/articulo-3">
-                <div class="grow">
-                  <span class="text-sm font-bold text-gray-800 group-hover:text-blue-600 group-focus:text-blue-600 dark:text-neutral-200 dark:group-hover:text-blue-500 dark:group-focus:text-blue-500">
-                    7 Principles of Icon Design
-                  </span>
-                </div>
-    
-                <div class="shrink-0 relative  mr-4  rounded-lg overflow-hidden size-20">
-                  <img class="size-full absolute top-0 start-0 object-cover rounded-lg" src="https://images.unsplash.com/photo-1586232702178-f044c5f4d4b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80" alt="Blog Image"/>
-                </div>
-              </a>
-
-              <a class="group flex items-center gap-x-6 focus:outline-none" href="/blog/articulo-4">
-                <div class="grow">
-                  <span class="text-sm font-bold text-gray-800 group-hover:text-blue-600 group-focus:text-blue-600 dark:text-neutral-200 dark:group-hover:text-blue-500 dark:group-focus:text-blue-500">
-                    7 Principles of Icon Design
-                  </span>
-                </div>
-    
-                <div class="shrink-0 relative  mr-4  rounded-lg overflow-hidden size-20">
-                  <img class="size-full absolute top-0 start-0 object-cover rounded-lg" src="https://images.unsplash.com/photo-1586232702178-f044c5f4d4b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80" alt="Blog Image"/>
-                </div>
-              </a>
-
-              <a class="group flex items-center gap-x-6 focus:outline-none" href="/blog/articulo-5">
-                <div class="grow">
-                  <span class="text-sm font-bold text-gray-800 group-hover:text-blue-600 group-focus:text-blue-600 dark:text-neutral-200 dark:group-hover:text-blue-500 dark:group-focus:text-blue-500">
-                    7 Principles of Icon Design
-                  </span>
-                </div>
-    
-                <div class="shrink-0 relative  mr-4  rounded-lg overflow-hidden size-20">
-                  <img class="size-full absolute top-0 start-0 object-cover rounded-lg" src="https://images.unsplash.com/photo-1586232702178-f044c5f4d4b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80" alt="Blog Image"/>
-                </div>
-              </a>
-           
-              <a class="group flex items-center gap-x-6 focus:outline-none" href="/blog/articulo-6">
-                <div class="grow">
-                  <span class="text-sm font-bold text-gray-800 group-hover:text-blue-600 group-focus:text-blue-600 dark:text-neutral-200 dark:group-hover:text-blue-500 dark:group-focus:text-blue-500">
-                    7 Principles of Icon Design
-                  </span>
-                </div>
-    
-                <div class="shrink-0 relative  mr-4  rounded-lg overflow-hidden size-20">
-                  <img class="size-full absolute top-0 start-0 object-cover rounded-lg" src="https://images.unsplash.com/photo-1586232702178-f044c5f4d4b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80" alt="Blog Image"/>
-                </div>
-              </a>
-                </div>
-                 
-              
-
-            </div>
-          </div>
-        </div>
-        {/* End Sidebar */}
+        <Sidebar/>
 
       </div>
     </div>
--- app/components/blog.jsx
+++ app/components/blog.jsx
@@ -10,69 +10,71 @@ const ElBlog = () => {
 </div>
 
 <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
-<a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg focus:outline-none focus:border-transparent focus:shadow-lg transition duration-300 rounded-xl p-5 dark:border-neutral-700 dark:hover:border-transparent dark:hover:shadow-black/40 dark:focus:border-transparent dark:focus:shadow-black/40" href="#">
+{/* Articulos */}
+<a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg focus:outline-none focus:border-transparent focus:shadow-lg transition duration-300 rounded-xl p-5 dark:border-neutral-700 dark:hover:border-transparent dark:hover:shadow-black/40 dark:focus:border-transparent dark:focus:shadow-black/40" href="/blog/articulo-1">
   <div class="aspect-w-16 aspect-h-11">
     <img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1633114128174-2f8aa49759b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Blog Image"/>
   </div>
   <div class="my-6">
     <h3 class="text-xl font-semibold text-gray-800 dark:text-neutral-300 dark:group-hover:text-white">
-      Announcing a free plan for small teams
+      Por que tu negocio necesita una pagina web profesional?
     </h3>
     <p class="mt-5 text-gray-600 dark:text-neutral-400">
-      At Wake, our mission has always been focused on bringing openness.
+      Descripcion del articulo 1
     </p>
   </div>
   <div class="mt-auto flex items-center gap-x-3">
-    <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Avatar"/>
+    <img class="size-8 rounded-full" src="/photo_2023-12-1817.45.05.jpeg" alt="Avatar"/>
     <div>
       <h5 class="text-sm text-gray-800 dark:text-neutral-200">By Daniel Passantino</h5>
     </div>
   </div>
 </a>
 
-<a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg focus:outline-none focus:border-transparent focus:shadow-lg transition duration-300 rounded-xl p-5 dark:border-neutral-700 dark:hover:border-transparent dark:hover:shadow-black/40 dark:focus:border-transparent dark:focus:shadow-black/40" href="#">
+<a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg focus:outline-none focus:border-transparent focus:shadow-lg transition duration-300 rounded-xl p-5 dark:border-neutral-700 dark:hover:border-transparent dark:hover:shadow-black/40 dark:focus:border-transparent dark:focus:shadow-black/40" href="/blog/articulo-2">
   <div class="aspect-w-16 aspect-h-11">
     <img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1562851529-c370841f6536?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Blog Image"/>
   </div>
   <div class="my-6">
     <h3 class="text-xl font-semibold text-gray-800 dark:text-neutral-300 dark:group-hover:text-white">
-      How Google Assistant now helps you record stories for kids
+    El poder del diseño grafico en tu marca personal
     </h3>
     <p class="mt-5 text-gray-600 dark:text-neutral-400">
-      Google is constantly updating its consumer AI, Google Assistant, with new features.
+      Descripcion del articulo 2
     </p>
   </div>
   <div class="mt-auto flex items-center gap-x-3">
-    <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Avatar"/>
+    <img class="size-8 rounded-full" src="/photo_2023-12-1817.45.05.jpeg" alt="Avatar"/>
     <div>
       <h5 class="text-sm text-gray-800 dark:text-neutral-200">By Daniel Passantino</h5>
     </div>
   </div>
 </a>
 
-<a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg focus:outline-none focus:border-transparent focus:shadow-lg transition duration-300 rounded-xl p-5 dark:border-neutral-700 dark:hover:border-transparent dark:hover:shadow-black/40 dark:focus:border-transparent dark:focus:shadow-black/40" href="#">
+<a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg focus:outline-none focus:border-transparent focus:shadow-lg transition duration-300 rounded-xl p-5 dark:border-neutral-700 dark:hover:border-transparent dark:hover:shadow-black/40 dark:focus:border-transparent dark:focus:shadow-black/40" href="/blog/articulo-3">
   <div class="aspect-w-16 aspect-h-11">
     <img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1521321205814-9d673c65c167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Blog Image"/>
   </div>
   <div class="my-6">
     <h3 class="text-xl font-semibold text-gray-800 dark:text-neutral-300 dark:group-hover:text-white">
-      Front accounts - let's work together
+     Tus redes sociales: El primer Impacto para conquistar nuevos clientes
     </h3>
     <p class="mt-5 text-gray-600 dark:text-neutral-400">
-      Are you an accountant? Are you a company formation advisor?
+      Descripcion del articulo 3
     </p>
   </div>
   <div class="mt-auto flex items-center gap-x-3">
-    <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Avatar"/>
+    <img class="size-8 rounded-full" src="/photo_2023-12-1817.45.05.jpeg" alt="Avatar"/>
     <div>
       <h5 class="text-sm text-gray-800 dark:text-neutral-200">By Daniel Passantino</h5>
     </div>
   </div>
 </a>
+{/* Articulos */}
 </div>
 
 <div class="mt-12 text-center">
-<a class="py-3 px-4 inline-flex bg-gradient-to-r from-purple-400 via-[#7d5ed7] to-[#683fe4]   items-center gap-x-1 text-sm font-medium rounded-full border border-gray-200 bg-white text-white shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800" href="#">
+<a class="py-3 px-4 inline-flex bg-gradient-to-r from-purple-400 via-[#7d5ed7] to-[#683fe4]   items-center gap-x-1 text-sm font-medium rounded-full border border-gray-200 bg-white text-white shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800" href="/blog">
   Ver mas
   <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
 </a>
